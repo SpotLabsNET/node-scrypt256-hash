e9926a3fad1161d5489d729f14a95184886ce921
Matthew Little <zone117x@gmail.com>
2014-01-07 18:27:56 -0500

    updated

--- test.js
+++
@@ -1,5 +0,0 @@
-var scrypthash = require('./build/Release/scrypthash');
-
-var buff = new Buffer('1dbd981fe6985776b644b173a4d0385ddc1aa2a829688d1e0000005645645645645645654645645645645456456456456456454564560000000000', 'hex');
-
-console.log(scrypthash.digest(buff));
